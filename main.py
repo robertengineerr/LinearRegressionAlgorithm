d3c02ba6707ef29e912b7780118c318ba493c9d7
EC2 Default User <ec2-user@ip-172-31-20-18.us-east-2.compute.internal>
2025-02-25 01:55:19 +0000

    Added separate project 1 file, will add another for project 2

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import sys
 
 from cleaning import clean_empty
 from regression import gradient_descent
+from proj1 import runProj1
 
 fileName="D3.csv"
 alpha=0.05
 iterations = 1000
 outputFileName = 'cost_history_plot'
+
+whichProj=1
 
 if len(sys.argv) > 1:
     for i in range(1, len(sys.argv)):
@@ -25,31 +28,17 @@ if len(sys.argv) > 1:
         elif sys.argv[i]=="-o":
             if i + 1 < len(sys.argv):
                 outputFileName = sys.argv[i+1]
+        elif sys.argv[i]=="-p":
+            if i + 1 < len(sys.argv):
+                whichProj = int(sys.argv[i+1])
         elif sys.argv[i] == "--help":
             print("-a   Alpha value (learning rate)")
             print("-i   Number of iterations")
             print("-f   Input csv file name")
             print("-o   Output plot file name")
 
-df = pd.read_csv(fileName, na_values="########")
-
-df = clean_empty(df)
-
-X = df[['X1', 'X2', 'X3']].values
-y = df['Y'].values
-
-X = np.column_stack((np.ones(X.shape[0]), X))  # adding a column of ones for the intercept. required for the linear regression formula
-
-#parameters
-theta_initial = np.zeros(X.shape[1])  # initial values for theta
-
-theta_final = gradient_descent(X, y, theta_initial, alpha, iterations)
-
-cost_df = pd.read_csv("cost_history.csv")
-plt.plot(cost_df['Iteration'], cost_df['Cost'])  # plotting the cost over iterations
-plt.xlabel('Iteration')
-plt.ylabel('Cost')
-plt.title(f'Cost History during Gradient Descent\nα = {alpha}, Iterations = {iterations}')
-plt.savefig(outputFileName+".png")
-
-print("final theta values:", theta_final)
+if whichProj==1:
+    runProj1(fileName, outputFileName, alpha, iterations)
+elif whichProj==2:
+    print("Not done yet")
+    #runProj2()
