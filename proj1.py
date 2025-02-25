06461a1931f57a8faf85e3747385ea943ba834eb
EC2 Default User <ec2-user@ip-172-31-20-18.us-east-2.compute.internal>
2025-02-25 01:57:00 +0000

    Adding project 1 file didn't work the first time for some reason

---
+++ proj1.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+from cleaning import clean_empty
+from regression import gradient_descent
+
+def runProj1(fileName, outputFileName, alpha, iterations):
+    df = pd.read_csv(fileName, na_values="########")
+
+    df = clean_empty(df)
+    
+    X = df[['X1', 'X2', 'X3']].values
+    y = df['Y'].values
+    
+    X = np.column_stack((np.ones(X.shape[0]), X))  # adding a column of ones for the intercept. required for the linear regression formula
+    
+    #parameters
+    theta_initial = np.zeros(X.shape[1])  # initial values for theta
+    
+    theta_final = gradient_descent(X, y, theta_initial, alpha, iterations)
+    
+    cost_df = pd.read_csv("cost_history.csv")
+    plt.plot(cost_df['Iteration'], cost_df['Cost'])  # plotting the cost over iterations
+    plt.xlabel('Iteration')
+    plt.ylabel('Cost')
+    plt.title(f'Cost History during Gradient Descent\nα = {alpha}, Iterations = {iterations}')
+    plt.savefig(outputFileName+".png")
+    
+    print("final theta values:", theta_final)
+    return
